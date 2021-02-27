5fb9cf0164c3d64402dee9c97410d495fe476b15
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-02-27 12:24:30 -0800

    Update config.php

--- config.php
+++ config.php
@@ -4,13 +4,13 @@
 
 $token = "ZTNhNDdjZjItY2VlNy00OWJkLThhMzItZTg0ZDc4OTcyMDNk";
 $uuid = "9b209f4ac3b68a5c";
-$userid = "37296628";
+$userid = "47789454";
 
-$eksternal_version = "2.0.1";
+$eksternal_version = "2.6.1.4";
 $version = "42";
-$device = "9";
+$device = "6.0";
 $timezone = "7";
-$lang = "id";
+$lang = "en";
 
 $spin_version = "40";
 $user_agent = "Mozilla/5.0 (Linux; Android 6.0; Lenovo A6600d40) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
