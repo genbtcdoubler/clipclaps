49170e69ceaf9c75bddd950fd211b1cd4e846de6
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-02-27 11:17:20 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "ZTNhNDdjZjItY2VlNy00OWJkLThhMzItZTg0ZDc4OTcyMDNk";
+$uuid = "9b209f4ac3b68a5c";
+$userid = "37296628";
+
+$eksternal_version = "2.0.1";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; Android 6.0; Lenovo A6600d40) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.181 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
